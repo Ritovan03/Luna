662bdd655114f997695d3dd87ce6f5bba96abd6e
Ritovan03 <ritovan0302@gmail.com>
2025-04-01 19:27:38 +0530

    add SensoryTherapyScreen route to navigation graph

--- app/src/main/java/com/example/mentalhealthapp/presentation/Navigation/Route.kt
+++ app/src/main/java/com/example/mentalhealthapp/presentation/Navigation/Route.kt
@@ -47,4 +47,6 @@ sealed class Route(
   object Chat: Route("chat")
 
     object Todo : Route("todo")
+    object ToDo : Route("todo")
+    object SensoryTherapy : Route("sensory_therapy")
 }
---
+++ app/src/main/java/com/example/mentalhealthapp/presentation/haven/govt/SensoryTherapyScreen1.kt
@@ -0,0 +1,10 @@
+package com.example.mentalhealthapp.presentation.haven.govt
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SensoryTherapyScreen1(mainNavController: NavHostController)
+{
+
+}
